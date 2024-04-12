8d60a1a43d273eb01e4bcd39776239d40d2d78cd
Dmitrii Shiptei <shi-java5@yandex.ru>
2024-04-12 16:41:21 +0300

    init

---
+++ src/main/java/com/example/project_tg_bot/ProjectTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_tg_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_tg_bot/ProjectTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_tg_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
