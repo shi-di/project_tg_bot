b45ad5fd23efc77bc2c4390c3e4f9abb0a16cfa0
Mangusti4ek <38989503+Mangusti4ek@users.noreply.github.com>
2024-04-13 17:47:29 +0700

    Create money service (#1)

---
+++ money_monitor_service/src/test/java/org/example/money_monitor_service/MoneyMonitorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.money_monitor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMonitorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
