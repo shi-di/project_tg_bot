1ecab78a7728f140c1a66372ef6fc5e92e2a7fef
Aleksandr <alexeremeev7@gmail.com>
2024-04-13 17:43:44 +0700

    Create money service

---
+++ money_monitor_service/src/main/java/org/example/money_monitor_service/MoneyMonitorServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.money_monitor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMonitorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyMonitorServiceApplication.class, args);
+    }
+
+}
